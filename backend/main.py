ddeaae12d13d6326accde687149b0b7bb084c156
Stepan <always.prog@gmail.com>
2023-11-18 15:58:11 +0300

    init search functionality

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-
+from flask import request
 # TODO:
 """
 1. Реализовать подключение к таблицам баз данных контрактов через SQLAlchemy.
@@ -14,5 +14,7 @@ def search_suppliers():
     TODO: Реализовать поиск по ИНН или по тексту продажи.
     TODO: Реализовать возврат в формате данных о поставщике вместе с его рейтигном (рейтинг надо считать)
     """
-    print(Supplier(632105575855, db.session).calc_supplier_rating(25, 25, 25, 25))
-    return f'supp {Supplier(632105575855, db.session).calc_supplier_rating(25, 25, 25, 25)}'
+    q = request.args.get('q')
+    print(q)
+
+    return ''
