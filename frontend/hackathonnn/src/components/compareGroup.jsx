f7d8166bb91a376386905182d92ffd16fb26c8e1
Stepan <always.prog@gmail.com>
2023-11-19 14:47:33 +0300

    better style for group

--- frontend/hackathonnn/src/components/compareGroup.jsx
+++ frontend/hackathonnn/src/components/compareGroup.jsx
@@ -1,30 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { baseUrl } from '../constants';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { baseUrl } from "../constants";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from "@mui/material";
 
 export default function CompareGroup() {
-  const {inns} = useParams();
+  const { inns } = useParams();
   const search = useLocation().search;
-  const kpgzs = new URLSearchParams(search).get('kpgzs');
+  const kpgzs = new URLSearchParams(search).get("kpgzs");
   const [response, setResponse] = useState();
 
   useEffect(() => {
-    fetch(`${baseUrl}/compare/bygroup?inns=${inns}${kpgzs ? '&'+'kpgzs='+kpgzs : ''}`).then(
-      res => res.json()
-    ).then(json => {
-      setResponse(json)
-    })
-  }, [])
+    fetch(
+      `${baseUrl}/compare/bygroup?inns=${inns}${
+        kpgzs ? "&" + "kpgzs=" + kpgzs : ""
+      }`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setResponse(json);
+      });
+  }, []);
 
   return (
-    <div>{response ? <div>
-      КПГЗ: {kpgzs}<br/>
-      {response.map(supplier => (<p>
-      ИНН: {supplier.inn} <br/>
-      Цена: {supplier.avg_price} <br/>
-    </p>))}
-
-    </div>: <div className="loading">Загрузка...</div>}</div>
-  )
+    <div>
+      {response ? (
+        <div>
+          КПГЗ: {kpgzs}
+          <br />
+          {response.map((supplier) => (
+            <Card sx={{ minWidth: 275, marginTop: 2 }}>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  ИНН: {supplier.inn}
+                </Typography>
+                <Typography variant="body2">
+                  Цена: {supplier.avg_price}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <div className="loading">Загрузка...</div>
+      )}
+    </div>
+  );
 }
+
