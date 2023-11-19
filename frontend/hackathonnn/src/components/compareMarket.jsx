654b07d24c32817cebe110e9158661f33e6d9826
Ekaterina <ekaterina.below@yandex.ru>
2023-11-19 14:31:49 +0300

    css for copare

--- frontend/hackathonnn/src/components/compareMarket.jsx
+++ frontend/hackathonnn/src/components/compareMarket.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { baseUrl } from '../constants';
+import{Box,Card, CardActions, CardContent, Button, Typography}from '@mui/material';
 
 export default function CompareMarket() {
   const {inn} = useParams();
@@ -20,11 +21,26 @@ export default function CompareMarket() {
 
   return (
     <div>{response ? (<div>
-      <div>ИНН: {inn}</div><br/>
+      {/* <div>ИНН: {inn}</div><br/>
       <div>Цена у поставщика: {response.target_avg_price}</div><br/>
-      <div>Цена по рынку: {response.market_avg_price}</div><br/>
-
-
+      <div>Цена по рынку: {response.market_avg_price}</div><br/> */}
+      <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+        ИНН: {inn}
+        </Typography>
+        <Typography variant="body2">
+        Цена у поставщика: {response.target_avg_price}
+        </Typography>
+        <Typography variant="body2">
+        Цена по рынку: {response.market_avg_price}
+        </Typography>
+      </CardContent>
+    </Card>
     </div>) : <div className="loading">Загрузка...</div>}</div>
+
+
+
+
   )
 }
